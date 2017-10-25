273aadfccb0cf343ed6f05884c31dbc20678b75b
TobiWainwright <31863771+TobiWainwright@users.noreply.github.com>
2017-10-25 13:28:15 -0500

    Update ThreeDigit.cpp

--- ThreeDigit.cpp
+++ ThreeDigit.cpp
@@ -35,17 +35,17 @@ void pause() {
  
 void main() {
 	int number; // X is a three digit number 
-	int digit_a; // hundreds place
-	int digit_b; // tens place
-	int digit_c; // ones place
+	int a; // hundreds place
+	int b; // tens place
+	int c; // ones place
 
 	cout << "Choose a 3 Digit number:" << endl;
 	cin >> number;
-	digit_a = number / 100;
-	digit_b = (number % 100) / 10;
-	digit_c = number % 10;
+	a = number / 100;
+	b = (number % 100) / 10;
+	c = number % 10;
 
-	if (digit_a > digit_b && digit_b > digit_c) {
+	if (a > b && b > c) {
 	cout << "descending";
 }
 	else if (digit_a < digit_b && digit_b < digit_c) {
